60307011ea5ee3001548281eaba6859886b5558b
Biraj Thapa <birajthapa983@gmail.com>
2024-05-14 15:15:21 +0545

    added tailwind css in project

--- ngit-task/src/App.jsx
+++ ngit-task/src/App.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const App = () => {
-  return <div>Hii </div>;
+  return <div >
+    <button className="px-3 py-4 bg-slate-600 rounded-2xl m-6">button</button>
+  </div>;
 };
 
 export default App;
