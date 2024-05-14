4a14be0afbf6b85e4c45c0cd65bd6fc43c35e02f
Biraj Thapa <birajthapa983@gmail.com>
2024-05-14 21:37:14 +0545

    different shapes added

---
+++ ngit-task/src/shapes/SubTite.jsx
@@ -0,0 +1,9 @@
+const SubTitle = ({ subTitleText }) => {
+    return subTitleText ? (
+      <div className="text-xs text-white">{subTitleText}</div>
+    ) : (
+      <div className="h-3 bg-gradient-to-br from-rose-400 to-rose-300 rounded w-1/2 shadow-lg"></div>
+    );
+  };
+  
+  export default SubTitle;
